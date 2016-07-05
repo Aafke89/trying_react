f874f04614b96eecc81305eb9052754126c3bd1b
Aafke Smal <aafke.smal@gmail.com>
2016-07-05 20:38:33 +0200

    Add delete-function

--- app/assets/javascripts/components/record.js.jsx
+++ app/assets/javascripts/components/record.js.jsx
@@ -1,10 +1,22 @@
 var Record = React.createClass({
+  handleDelete: function(e) {
+    // e.preventDefault();
+    $.ajax( {
+      method: 'DELETE',
+      url: '/records/' + this.props.record.id,
+      dataType: 'JSON',
+      success: function() {
+        this.props.handleDeleteRecord(this.props.record)
+      }.bind(this)
+    });
+    },
   render: function(){
     return(
       <tr>
         <td>{ this.props.record.date }</td>
         <td>{ this.props.record.title }</td>
         <td>{ this.props.record.amount }</td>
+        <td><a className='btn btn-danger' onClick={this.handleDelete}>delete</a></td>
       </tr>
     )
   }
--- app/assets/javascripts/components/records.js.jsx
+++ app/assets/javascripts/components/records.js.jsx
@@ -14,10 +14,40 @@ var Records = React.createClass({
     records.push(record);
     this.setState({ records: records });
   },
+   deleteRecord: function(record){
+    var records = this.state.records.slice();
+    var index = records.indexOf(record);
+      records.splice(index, 1);
+        this.replaceState({records: records});
+  },
+  credits: function(){
+    var credits = this.state.records.filter(function(val) {
+      return val.amount >= 0
+    });
+    return credits.reduce(function(prev, curr) {
+      return prev + parseFloat(curr.amount);
+    }, 0)
+  },
+   debits: function(){
+    var debits = this.state.records.filter(function(val) {
+      return val.amount < 0
+    });
+    return debits.reduce(function(prev, curr) {
+      return prev + parseFloat(curr.amount);
+    }, 0)
+  },
+  balance: function(){
+    return this.debits() + this.credits();
+  },
   render: function(){
   return(
   <div className='records'>
     <h2 className='title'> Records </h2>
+    <div className='row'>
+      <AmountBox type='success' amount={this.credits()} text='Credit' />
+      <AmountBox type='danger' amount={this.debits()} text='Debit' />
+      <AmountBox type='info' amount={this.balance()} text='Balance' />
+    </div>
     <RecordForm handleNewRecord={this.addRecord} />
     <table className='table'>
       <thead>
@@ -25,12 +55,14 @@ var Records = React.createClass({
           <th>Date</th>
           <th>Title</th>
           <th>Amount</th>
+          <th>Actions</th>
         </tr>
       </thead>
     <tbody>
-      { this.state.records.map(function(record){
-      return <Record record={record} />
-      })}
+      {this.state.records.map(function(record){
+      return <Record record key={record.id} record={record}
+      handleDeleteRecord={this.deleteRecord} />
+      }.bind(this))}
       </tbody>
       </table>
     </div>
